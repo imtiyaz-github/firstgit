3e37203f25a223255878d4ed71517d9d6522cb03
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-02-14 10:22:17 +0530

    first commit

---
+++ Git.js
@@ -0,0 +1,12 @@
+
+
+
+
+items[3].style.backgroundColor ="green";
+
+for(var i=0;i<items.length;i++) {
+    items[i].style.fontWeight ="bold";
+}
+
+
+console.log(document.getElementsByName(items));
---
+++ task3.js
@@ -0,0 +1,19 @@
+
+
+
+
+console.log(document.getElementsByClassName("container"))
+
+
+//gives an error
+//items[3].style.backgroundColor = "green";
+
+
+
+for(var i=0;i<items.length;i++) {
+    items[i].style.fontWeight ="bold";
+}
+
+
+console.log(document.getElementsByName(items));
+
