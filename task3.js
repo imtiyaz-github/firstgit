52805c85d781479c3f3902b83b1d208e401ef032
imtiyazgithub <imtiyazshirasangi456@gmail.com>
2023-02-14 14:39:29 +0530

    first commit

--- task3.js
+++ task3.js
@@ -2,18 +2,22 @@
 
 
 
-console.log(document.getElementsByClassName("container"))
+var li  =  document.getElementsByTagName('li');
 
 
-//gives an error
-//items[3].style.backgroundColor = "green";
+console.log(li);
+
+console.log(li[1]);
+console.log(document.getElementsByClassName("list-group-item"));
+
+console.log(document.getElementsByTagName('items'));
+console.log(document.getElementsByClassName('items are here'));
+
+li[1].textContent = 'hello2';
+li[1].style.fontWeight  ="bold";
+li[1].style.backgroundColor = 'pink';
 
 
-
-for(var i=0;i<items.length;i++) {
-    items[i].style.fontWeight ="bold";
+for(var  i =0;i<items.length;i++) {
+    items[i].style.backgroundColor = 'green';
 }
-
-
-console.log(document.getElementsByName(items));
-
