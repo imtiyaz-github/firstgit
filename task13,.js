cb084e1a69017fa14f22a18e32d074d4539e10c6
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-02-22 12:21:38 +0530

    added task13.js

--- task13,.js
+++ task13,.js
@@ -1,48 +1,40 @@
-// function saveToLocalStorage(event) {
-//     event.preventDefault();
-//     const name = event.target.name.value;
-//     const email  = event.target.email.value;
-//     const phonenumber = event.target.phonenumber.value;
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email  = event.target.email.value;
+    const phonenumber = event.target.phonenumber.value;
 
-//     const object = {
-//         name,email,phonenumber
-//     };
-
-
-//     localStorage.setItem(email,JSON.stringify('object'));
+    const object = {
+        name,email,phonenumber
+    };
 
 
-//     showUserOnScreen(object,email);
-// }
-
-// function showUserOnScreen(object,email) {
-//     const parentElem =  document.getElementById('list');
-//     const newUser = document.createElement('li')
-//     newUser.textContent = object.name+ ','+object.email+','+object.phonenumber;
+    localStorage.setItem(email,JSON.stringify('object'));
 
 
+    showUserOnScreen(object,email);
+}
 
-// //create a delete button
-
-// var deleteBtn = document.createElement('input');
-// deleteBtn.type = 'button';
-// deleteBtn.value = 'Delete';
-// deleteBtn.className = 'btn btn-warning'
-
-
-// deleteBtn.onclick=()=> {
-//     localStorage.removeItem(email);
-//     parentElem.removeChild(newUser);
-
-// }
-// newUser.appendChild(deleteBtn);
-// parentElem.appendChild(newUser);
-// }
-
-
-// console.log('hello');
+function showUserOnScreen(object,email) {
+    const parentElem =  document.getElementById('list');
+    const newUser = document.createElement('li')
+    newUser.textContent = object.name+ ','+object.email+','+object.phonenumber;
 
 
 
+//create a delete button
+
+var deleteBtn = document.createElement('input');
+deleteBtn.type = 'button';
+deleteBtn.value = 'Delete';
+deleteBtn.className = 'btn btn-warning'
 
 
+deleteBtn.onclick=()=> {
+    localStorage.removeItem(email);
+    parentElem.removeChild(newUser);
+
+}
+newUser.appendChild(deleteBtn);
+parentElem.appendChild(newUser);
+}
