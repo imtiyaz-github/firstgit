26e2577acc0a423538c8ef7ce18e923dd033a4bc
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-02-22 12:09:20 +0530

    your message goes here

---
+++ project/calculator/task13,.js
@@ -0,0 +1,54 @@
+// function saveToLocalStorage(event) {
+//     event.preventDefault();
+//     const name = event.target.name.value;
+//     const email  = event.target.email.value;
+//     const phonenumber = event.target.phonenumber.value;
+
+//     const object = {
+//         name,email,phonenumber
+//     };
+
+
+//     localStorage.setItem(email,JSON.stringify('object'));
+
+
+//     showUserOnScreen(object,email);
+// }
+
+// function showUserOnScreen(object,email) {
+//     const parentElem =  document.getElementById('list');
+//     const newUser = document.createElement('li')
+//     newUser.textContent = object.name+ ','+object.email+','+object.phonenumber;
+
+
+
+// //create a delete button
+
+// var deleteBtn = document.createElement('input');
+// deleteBtn.type = 'button';
+// deleteBtn.value = 'Delete';
+// deleteBtn.className = 'btn btn-warning'
+
+
+// deleteBtn.onclick=()=> {
+//     localStorage.removeItem(email);
+//     parentElem.removeChild(newUser);
+
+// }
+// newUser.appendChild(deleteBtn);
+// parentElem.appendChild(newUser);
+// }
+
+
+// console.log('hello');
+
+
+
+var element;
+
+element  = document.getElementsByClassName('btn btn-dark');
+console.log(element);
+
+
+
+
